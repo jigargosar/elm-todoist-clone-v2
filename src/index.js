9c617dc2cb5e7fdad1362baa10288eeef7a7e5bb
Jigar Gosar <jigar.gosar@gmail.com>
2019-09-25 19:00:54 +0530

    init

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import './index.css'
 import { Elm } from './Main.elm'
 
 const app = Elm.Main.init({
-  flags: { cache: localStorage.getItem("elm-todoist-clone-v2-cache") , now: Date.now()},
+  flags: {
+    cache: localStorage.getItem('elm-todoist-clone-v2-cache'),
+    now: Date.now(),
+  },
   node: document.getElementById('root'),
 })
 
-
-app.ports.setCache.subscribe(cacheString=> localStorage.setItem("elm-todoist-clone-v2-cache", cacheString))
+app.ports.setCache.subscribe(cacheString =>
+  localStorage.setItem('elm-todoist-clone-v2-cache', cacheString),
+)
