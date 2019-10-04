334d3523cf9d8913985b5fb2e826bfed58c48b4c
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-04 15:12:04 +0530

    ui explorer

--- src/Explorer.elm
+++ src/Explorer.elm
@@ -22,8 +22,8 @@ update message model =
 
 view : Model -> Html Msg
 view _ =
-    div [ class "pa3 flex", style "min-height" "vh100" ]
-        [ text "Hello" ]
+    div [ class "pa3 flex items-center bg-washed-red", style "min-height" "100vh" ]
+        [ div [] [ text "Hello" ] ]
 
 
 main : Program {} Model Msg
