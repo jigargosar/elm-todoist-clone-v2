ef32fb42c6f5fa22e71b8d6e23107bfa4126513f
Jigar Gosar <jigar.gosar@gmail.com>
2019-10-04 15:05:35 +0530

    ui explorer

--- src/Explorer.elm
+++ src/Explorer.elm
@@ -1,21 +1,26 @@
 module Explorer exposing (main)
 
-import Button exposing (Appearance(..), Kind(..), Size(..), defaultButtonConfig)
-import UIExplorer exposing (UIExplorerProgram, defaultConfig, explore, storiesOf)
+import Browser
+import Html.Styled exposing (..)
 
 
-main : UIExplorerProgram {} () {}
+type alias Model =
+    {}
+
+
+type Msg
+    = NoOp
+
+
+update message model =
+    ( model, Cmd.none )
+
+
+view _ =
+    toUnstyled <|
+        text "HH"
+
+
+main : Program {} Model Msg
 main =
-    explore
-        defaultConfig
-        [ storiesOf
-            "Button"
-            [ ( "Primary", \_ -> Button.view "Submit" defaultButtonConfig (), {} )
-            , ( "Secondary", \_ -> Button.view "Submit" { defaultButtonConfig | appearance = Secondary } (), {} )
-            , ( "Small", \_ -> Button.view "Submit" { defaultButtonConfig | size = S } (), {} )
-            , ( "Large", \_ -> Button.view "Submit" { defaultButtonConfig | size = L } (), {} )
-            , ( "Link", \_ -> Button.view "Submit" { defaultButtonConfig | kind = Link, appearance = Secondary } (), {} )
-            , ( "GhostPrimary", \_ -> Button.view "Submit" { defaultButtonConfig | kind = Ghost } (), {} )
-            , ( "GhostSecondary", \_ -> Button.view "Submit" { defaultButtonConfig | appearance = Secondary, kind = Ghost } (), {} )
-            ]
-        ]
+    Browser.element { init = \_ -> ( Model, Cmd.none ), view = view, update = update, subscriptions = \_ -> Sub.none }
